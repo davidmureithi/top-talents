930a3bbb4048adc7975257c1d39e7a360fbb011d
David <ndekeredavid@gmail.com>
2016-12-16 14:55:37 +0300

    Added Contact Page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Single Page</title>
+		<title>Top Talents Landing Page</title>
 		<link href="styles.css" rel="stylesheet" type="text/css">
  		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	</head>
@@ -16,7 +16,7 @@
                     <ul class="">
 					<li><a class="active" href="index.php">Home </a></li>
                         <li><a href="about-us.php" >About Us </a></li>
-                        <li><a href="#" >Contact Us </a></li>
+                        <li><a href="contact-us.php" >Contact Us </a></li>
                     </ul>
                 </nav>
             </div>
