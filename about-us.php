b001fbe8396c8be80a11c7599dab9a3d82070a94
David <ndekeredavid@gmail.com>
2016-12-16 14:37:40 +0300

    Top Talents

---
+++ about-us.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Single Page</title>
+        <link href="styles.css" rel="stylesheet" type="text/css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    </head>
+    <body style="margin:2em;">
+    <div id="home" class="top-image">
+        <header>
+            <div class="main-body">
+                <nav>
+                    <a href="index.php">
+<!--                         <img class="pull-left logo" src="images/logo.png" alt="Logo">
+ -->                    </a>
+                    <ul class="">                        
+                        <li><a class="active" href="index.php">Home </a></li>
+                        <li><a href="about-us.php" >About Us </a></li>
+                        <li><a href="#" >Contact Us </a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="spotlight">
+                <h1>We Strive for excellence</h1>
+            </div>
+        </header>
+    </div>
+    <section>
+        <div id="content" class="site-content">
+            <div class="container-fluid"style="padding:0;">
+                <div class="col-lg-12" style="padding:0;">
+                    <div class=" pull-left col-md-7" style="padding:0 50px;">
+                        <div class="pull-left">
+                            <hr class="pull-right hr-red">
+                            <div class="clearfix"></div>
+                            <span class="top-talent">Top Talents </br> Academy</span>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="pull-left">
+                            <p class="pull-left paragraph">
+                                Top Talents was established in January year 2004, with an aim of providing quality education  and holistic growth and development to the Kenyan child and Africa at large. </br>
+                                Top Talents service units include: </br>
+                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Local curriculum,basic education </br>
+                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;International curriculum </br>
+                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Foreign languages, music and art</br>
+                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;International exchange programs and many more.</br>
+                                The school values include:</br>
+                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Integrity </br>
+                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Responsibility and accountability </br>
+                                These values have enabled stakeholders to achieve high consistent performance in National Examinations </br>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="pull-right col-md-offset-1 col-md-4" style="padding:0;">
+                        <div class="insurance-subscribe">
+                            <div class="quote">
+                                <div class="past-results">
+                                    <span>Download Past  <br>  KCPE Results</span>
+                                </div>
+                                <span><img style=" width:50%; margin: 2em 7.5em;" src="images/book.png" alt="Logo"></span>
+                                <input type="submit" name="vehicle" value="Past Results">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="photos">
+            <img src="images/parade.png" alt="Logo">
+            <img src="images/classrooms.png" alt="Logo">
+            <img src="images/children_parade.png" alt="Logo">
+            <img src="images/bus2.png" alt="Logo">
+            <img src="images/playing_soccer.png" alt="Logo">
+            <img src="images/classrooms_back.png" alt="Logo">
+            <img src="images/attention.png" alt="Logo">
+            <img src="images/girls_singing.png" alt="Logo">
+    </section>
+
+    <footer>
+        <div class="home-footer">
+            <div class="page-wrapper" style="margin: 0 10%">
+                <div class="col-lg-12">
+                    <div class="footercontent">
+                        <div class="copyright pull-left">
+                        <!--    &copy;  &#169;  &#xA9;  &#9400;    -->                            
+                        <span>&#9400; 2016. All Rights Reserved. Designed by Tatu Creatives Ltd.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/4.0.2/bootstrap-material-design.iife.js" type="text/javascript"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Single Page</title>
+		<link href="styles.css" rel="stylesheet" type="text/css">
+ 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="col-md-12" id="Background">
+            <div class="main-body" style="margin-top:30px; margin-left: 50px;">
+                <nav>
+                    <a href="index.php">
+<!--                         <img class="pull-left logo" src="images/logo.png" alt="Logo">
+ -->                    </a>
+                    <ul class="">
+					<li><a class="active" href="index.php">Home </a></li>
+                        <li><a href="about-us.php" >About Us </a></li>
+                        <li><a href="#" >Contact Us </a></li>
+                    </ul>
+                </nav>
+            </div>
+			<div class="col-md-6">
+				<div class="" style="padding: 170px 0;">
+					<span class="" style=" margin-left: -30px; background: rgba(238, 238, 238, 0.46); padding: 40px 0 42px 30px;">
+						<img class="" style="" width="15px" src="images/Arrow.png">
+						<a class="" href="about-us.php"><span class="" style="margin:20px; font-size:18px;"> ABOUT US </span></a>
+						<img class="" width="150px" src="images/Africanboy.png">
+					</span>
+				</div>
+			</div>
+			<div class="col-md-6" style="margin-top: -150px;">
+				<div style="margin: 100px;">
+					<img id="BluePattern" width="390px" height="420px" src="images/Bluebg.png">
+					<img id="Pattern" width="390px" height="420px" src="images/Pattern.png">
+					<div class="Form">
+						<h4 class="join">are you interested in joining us? <br> kindly fill in your details below</h4>
+						<div class="field-email">
+						    <input type="text" class="field-email" id="" placeholder="Name">
+						</div>
+						<div class="field-email">
+						    <input type="email" class="field-email" id="" placeholder="Email address">
+						</div>
+						<div class="field-email">
+						    <input type="tel" class="field-email" id="" placeholder="Phone Number">
+						</div>
+						<div class="field-email">
+						    <label>Please select your class</label>
+							<select>
+							  	<option value="Class 5">Class 5</option>
+							  	<option value="Class 6">Class 6</option>
+							  	<option value="Class 7">Class 7</option>
+							  	<option value="Class 8">Class 8</option>
+							</select>
+						</div>
+						<br>							
+						<div class="field-email">
+					  		<button type="submit" class="field-email">SEND</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+ 	</body>
+ 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/4.0.2/bootstrap-material-design.iife.js" type="text/javascript"></script>
+ </html>
